e2bb4b46fa666744388da015f8523d50ee962577
Andrew Ball <andrew@nordicgamelab.org>
2023-06-16 13:27:21 -0400

    added checkout form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+let modalMain = document.getElementById('modal_mainContent');
 // open modal by id
 function openModal(id) {
   document.getElementById(id).classList.add('open');
@@ -18,3 +20,39 @@ window.addEventListener('load', function() {
       }
   });
 });
+
+function checkout() {
+  modalMain.style.display = 'none';
+};
+
+// Handle form submission
+const form = document.getElementById('checkoutForm');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+
+  // Send the form data to the serverless function
+  fetch('/.netlify/functions/submitCheckout', {
+    method: 'POST',
+    body: JSON.stringify({ name, email })
+  })
+    .then(response => {
+      if (response.ok) {
+        console.log('Form data submitted successfully!');
+        // You can perform additional actions here (e.g., show a success message)
+      } else {
+        console.error('Error submitting form data.');
+        // You can handle errors here (e.g., show an error message)
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      // You can handle errors here (e.g., show an error message)
+    });
+
+  // Close the modal after form submission (you can modify this behavior)
+  modal.style.display = 'none';
+});
+
