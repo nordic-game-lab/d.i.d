71bfde404cb08434b776b60f04990912864729c4
Andrew Ball <andrew@nordicgamelab.org>
2023-08-19 22:57:00 -0400

    update index-scripts.js

--- js/index-scripts.js
+++ js/index-scripts.js
@@ -19,14 +19,14 @@
 
 	//jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
-		$('.navbar-nav li a').bind('click', function(event) {
+		$('.navbar-nav li a').on('click', function(event) {
 			var $anchor = $(this);
 			$('html, body').stop().animate({
 				scrollTop: $($anchor.attr('href')).offset().top
 			}, 1500, 'easeInOutExpo');
 			event.preventDefault();
 		});
-		$('.page-scroll a').bind('click', function(event) {
+		$('.page-scroll a').on('click', function(event) {
 			var $anchor = $(this);
 			$('html, body').stop().animate({
 				scrollTop: $($anchor.attr('href')).offset().top
