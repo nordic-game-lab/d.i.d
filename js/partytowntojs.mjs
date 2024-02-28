9984571b07a13d3e6796117962e519f3f8759d74
Andrew Ball <andrew@nordicgamelab.org>
2024-02-28 19:41:22 +0000

    dslkfk.s

--- js/partytowntojs.mjs
+++ js/partytowntojs.mjs
@@ -1,4 +1,4 @@
-import { partytownSnippet } from '@builder.io/partytown/integration';
+import { partytownSnippet } from 'https://cdn.jsdelivr.net/npm/@builder.io/partytown@0.9.2/integration/index.mjs';
 const snippetText = partytownSnippet();
 const el = document.createElement('script');
 el.innerText = snippetText;
