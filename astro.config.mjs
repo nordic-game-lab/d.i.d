dbc3d4e70db87d2701e6944630060f710f2d4dd7
hi5a <okbomer999@gmail.com>
2024-10-03 01:07:39 +0000

    add about page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 
 import partytown from "@astrojs/partytown";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()]
+  integrations: [partytown(), tailwind()]
 });
